bf3a8506f0b13f29180f7ba35463aa1d66c8db8d
joshpetit <joshuapetitma@yahoo.com>
2020-06-11 15:24:04 -0400

    Added text to Speech

--- src/main/java/OutputPanel.java
+++ src/main/java/OutputPanel.java
@@ -1,11 +1,25 @@
 
+import marytts.LocalMaryInterface;
+import marytts.exceptions.MaryConfigurationException;
+import marytts.exceptions.SynthesisException;
+import marytts.util.data.audio.AudioPlayer;
+import org.apache.http.util.ByteArrayBuffer;
+
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import java.util.concurrent.locks.Condition;
 
 public class OutputPanel extends JPanel {
     private JLabel systemOutput;
     private JTextArea userOutput;
     private boolean capitalize = false;
+    private LocalMaryInterface maryInterface;
+    Clip clip;
+    private Thread speech;
+    private String lastMessage;
+
     final Font COMIC_SANS = new Font("Comic Sans", Font.BOLD, 30);
     public OutputPanel(){
         super(new GridBagLayout());
@@ -39,10 +53,42 @@ public class OutputPanel extends JPanel {
     }
 
     public void initSpeech(){
+        try {
+            maryInterface = new LocalMaryInterface();
 
+        } catch (MaryConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
+    //Find a way to prevent the words spoken from tripping over each other
     public void speak(String message){
+        if (clip != null && clip.isOpen()){
+            clip.close();
+            System.out.println("Due?");
+        }
+         speech = new Thread(() -> {
+            try {
+                AudioInputStream ais = maryInterface.generateAudio(message);
+                clip = AudioSystem.getClip();
+                clip.open(ais);
+                clip.start();
+                if (Thread.currentThread().isInterrupted()){
+                    clip.close();
+                }
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            } catch (SynthesisException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+            }
+
+        });
+        speech.start();
+
     }
 
     public void resetMap(){
@@ -65,16 +111,24 @@ public class OutputPanel extends JPanel {
             userOutput.append("\t");
         }
         else if( message.equalsIgnoreCase("Capitalize")){
-            capitalize = capitalize ? false: true;
+            capitalize = !capitalize;
         }
         else{
             message = capitalize ? message.toUpperCase(): message;
             userOutput.append(message);
         }
-        speak(message);
+        //In the case of constant repeated letters
+            speak(message);
+        lastMessage = message;
     }
 
     public String getText(){ return userOutput.getText();}
 
     public void systemMessage(String message){systemOutput.setText(message);}
+
+
+
 }
+
+
+
